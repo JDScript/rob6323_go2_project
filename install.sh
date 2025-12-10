938d7d214bec5a5822fdb9f9fd5f71d02c6b8f4c
JDScript <jd@jdscript.net>
2025-12-10 00:35:42 -0500

    chore: remove useless script and add IsaacLab as submodule

--- install.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-mkdir logs
-ssh -o StrictHostKeyChecking=accept-new burst "cd ~/rob6323_go2_project && sbatch --job-name='rob6323_${USER}' --mail-user='${USER}@nyu.edu' install.slurm '$@'"
-cd /scratch/$USER
-git clone https://github.com/isaac-sim/IsaacLab.git
